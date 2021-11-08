bb43d0b9a6e2e3da2605b449a33ec7035efad503
pantherblack34 <89083500+pantherblack34@users.noreply.github.com>
2021-11-08 23:14:42 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import location from './image/map(1).png'
+import temp from './image/fahrenheit.png'
+import humid from './image/humidity.png'
+import press from './image/thermometer.png'
+import wind from './image/windsock.png'
 
 function App() {
+  const apiKey = 'ec69eaab7d1dd592c2a334637808a1a4'
+
+  const [weather, setWeather] = useState({})
+  const [city, setCity] = useState('')
+
+  const getWeather = (event) => {
+    if(event.key === 'Enter'){
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+      .then(res => res.json())
+      .then(data => {
+        setCity('')
+        setWeather(data)
+        console.log(data)
+      })
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>Weather Status</h1>
+        <input 
+        type='text'
+        className='input'
+        placeholder='Enter the City....' 
+        onChange={e => setCity(e.target.value)}
+        value={city}
+        onKeyPress={getWeather} />
+        
+       <div className="weather-info">
+         {
+          (typeof weather.main === 'undefined') ? 
+          <h2>ENTER A PLACE</h2>
+          :
+          <div>
+            <div className='content1'><img src={location} alt='location' /> {weather.name}, {weather.sys.country}</div>
+            <div className='content2'><img src={temp} alt='temperature' /> {Math.round(weather.main.temp)}</div>
+            <div className='content3'><img src={humid} alt='humidity' /> {weather.main.humidity}%</div>
+            <div className='content4'><img src={press} alt='pressure' /> {weather.main.pressure}</div>
+            <div className='content5'><img src={wind} alt='windspeed' /> {weather.wind.speed}</div>
+          </div>
+         }
+       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
